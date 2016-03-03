f01cdc7d02e6aa659d47d518a1d46f4f28ef3077
matt123337@hotmail.com <matt123337@hotmail.com>
2016-03-02 20:29:35 -0500

    y does everything ctd?

--- gAyPI/gAyPI/ModLoader/LocalModLoader.cs
+++ gAyPI/gAyPI/ModLoader/LocalModLoader.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Castle.Core.Internal;
 using gAyPI.StardewValley;
 
 namespace gAyPI.ModLoader
@@ -31,15 +32,8 @@ namespace gAyPI.ModLoader
 
         private void LoadMod(List<Assembly> result, string path)
         {
-            string[] files = Directory.GetFiles(path, "*.dll");
-            foreach (var str in files)
-            {
-                var asm = Assembly.LoadFile(str);
-                foreach (var mod in asm.Modules)
-                {
-                    mod.CustomAttributes.SingleOrDefault(t => t.AttributeType == typeof (Mod));
-                }
-            }
+            var files = Directory.GetFiles(path, "*.dll");
+            result.AddRange(from str in files select Assembly.LoadFile(str) into asm from mod in asm.Modules from type in mod.GetTypes() where type.CustomAttributes.FirstOrDefault(t => t.AttributeType == typeof (Mod)) != null select asm);
         }
     }
 }
