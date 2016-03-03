7e9757612333afbfa983f1713852aea009dda035
Cody <notdemmonic@hotmail.com>
2016-03-03 04:48:14 -0500

    bleh

---
+++ gAyPI/gAyPI/StardewValley/Wrapper/Character.cs
@@ -0,0 +1,71 @@
+﻿using gAyPI.StardewValley.Accessor;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gAyPI.StardewValley.Wrapper
+{
+    public class Character
+    {
+        private CharacterAccessor accessor;
+
+        public Character(CharacterAccessor accessor)
+        {
+            this.accessor = accessor;
+        }
+
+        public bool IsEmoteFading()
+        {
+            return accessor._IsEmoteFading();
+        }
+
+        public float GetEmoteInterval()
+        {
+            return accessor._GetEmoteInterval();
+        }
+
+        public Vector2 GetLastClick()
+        {
+            return accessor._GetLastClick();
+        }
+
+        public string GetName()
+        {
+            return accessor._GetName();
+        }
+
+        public Vector2 GetPosition()
+        {
+            return accessor._GetPosition();
+        }
+
+        public void SetPosition(Vector2 pos)
+        {
+            accessor._SetPosition(pos);
+        }
+
+        public float GetVelocityX()
+        {
+            return accessor._GetVelocityX();
+        }
+
+        public float GetVelocityY()
+        {
+            return accessor._GetVelocityY();
+        }
+
+        public float GetScale()
+        {
+            return accessor._GetScale();
+        }
+
+        public int GetSpeed()
+        {
+            return accessor._GetSpeed();
+        }
+
+    }
+}
---
+++ gAyPI/gAyPI/StardewValley/Wrapper/GameLocation.cs
@@ -0,0 +1,24 @@
+﻿using gAyPI.StardewValley.Accessor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gAyPI.StardewValley.Wrapper
+{
+    public class GameLocation
+    {
+        private GameLocationAccessor accessor;
+
+        public GameLocation(GameLocationAccessor accessor)
+        {
+            this.accessor = accessor;
+        }
+
+        public bool IsOutdoors()
+        {
+            return accessor._IsOutdoors();
+        }
+    }
+}
