433ef7567040a53fa983270e8b543705bd22ad92
Cody <notdemmonic@hotmail.com>
2016-03-05 17:11:45 -0500

    branch test

---
+++ Storm/Storm/StardewValley/ResourceHookMod.cs
@@ -0,0 +1,14 @@
+﻿using Storm.ExternalEvent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storm.StardewValley
+{
+    [Mod(Author = "Demmonic", Name = "Resource Hooker", Version = 0.1D)]
+    public class ResourceHookMod
+    {
+    }
+}
