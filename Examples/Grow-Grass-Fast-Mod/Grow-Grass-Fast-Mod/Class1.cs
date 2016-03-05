c3a2f14275ed4c7b5100a19a80d2c3e6fd3db61e
Cody <notdemmonic@hotmail.com>
2016-03-05 17:15:01 -0500

    wrong branch? gaaah

---
+++ Examples/Grow-Grass-Fast-Mod/Grow-Grass-Fast-Mod/Class1.cs
@@ -0,0 +1,24 @@
+﻿using Storm.ExternalEvent;
+using Storm.StardewValley.Event;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grow_Grass_Fast_Mod
+{
+    [Mod(Author = "Demmonic", Name = "Grow Grass Fast", Version = 0.1D)]
+    public class GrowGrassFastMod
+    {
+        [Subscribe]
+        public void PerformClockUpdateCallback(PerformClockUpdateEvent @event)
+        {
+            var loc = @event.Root.CurrentLocation;
+            if (loc != null)
+            {
+                loc.GrowWeedGrass(5);
+            }
+        }
+    }
+}
