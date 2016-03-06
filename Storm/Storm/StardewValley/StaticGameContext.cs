a39dd80ba26662e2c98d2dd3f63632979422ad8d
Russell Long <russell._long@hotmail.com>
2016-03-06 12:21:28 +1100

    made it pass crop to event

--- Storm/Storm/StardewValley/StaticGameContext.cs
+++ Storm/Storm/StardewValley/StaticGameContext.cs
@@ -190,7 +190,7 @@ namespace Storm.StardewValley
 
         public static DetourEvent CompleteGrowthCallback(CropAccessor accessor)
         {
-            var @event = new CompleteGrowthEvent();
+            var @event = new CompleteGrowthEvent(new Crop(accessor));
             EventBus.Fire<CompleteGrowthEvent>(@event);
             return @event;
         }
