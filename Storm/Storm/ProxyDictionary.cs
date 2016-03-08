dd9414d5fc0faa99a99c18f9d03acfe946d3f061
Cody <notdemmonic@hotmail.com>
2016-03-08 01:58:03 -0500

    added some rpoxy stuffs

---
+++ Storm/Storm/ProxyDictionary.cs
@@ -0,0 +1,43 @@
+﻿using Storm.StardewValley.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storm
+{
+    public class ProxyDictionary<TKey, TValue, TWrapper> 
+    {
+        public delegate W Wrap<V, W>(V val);
+
+        private System.Collections.IDictionary real;
+        private Wrap<TValue, TWrapper> wrapper;
+
+        public ProxyDictionary(System.Collections.IDictionary real, Wrap<TValue, TWrapper> wrapper)
+        {
+            this.real = real;
+            this.wrapper = wrapper;
+        }
+
+        public int Count
+        {
+            get { return real.Count; }
+        }
+
+        public void Add<T>(TKey key, Wrapper<T> value)
+        {
+            real.Add(key, value.Expose());
+        }
+
+        public void Remove(TKey key)
+        {
+            real.Remove(key);
+        }
+
+        public TWrapper Get(TKey key)
+        {
+            return wrapper((TValue) real[key]);
+        }
+    }
+}
