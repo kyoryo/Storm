7d8a17783f5cfa9e13120abf7e47b5346b8e7e59
Zoryn Aaron <zoryn4163@gmail.com>
2016-03-08 03:56:17 -0500

    adds 2 collision events

---
+++ Storm/Storm/StardewValley/Event/Character/ShouldCollideWithBuildingLayerEvent.cs
@@ -0,0 +1,15 @@
+﻿using Storm.StardewValley.Accessor;
+using Storm.StardewValley.Wrapper;
+
+namespace Storm.StardewValley.Event.Farmer
+{
+    public class ShouldCollideWithBuildingLayerEvent : StaticContextEvent
+    {
+        public GameLocation BuildingLocation { get; }
+
+        public ShouldCollideWithBuildingLayerEvent(GameLocationAccessor gameLocationAccessor)
+        {
+            BuildingLocation = new GameLocation(StaticGameContext.WrappedGame, gameLocationAccessor);
+        }
+    }
+}
---
+++ Storm/Storm/StardewValley/Event/Farmer/FarmerCollideWithEvent.cs
@@ -0,0 +1,15 @@
+﻿using Storm.StardewValley.Accessor;
+using Storm.StardewValley.Wrapper;
+
+namespace Storm.StardewValley.Event.Farmer
+{
+    public class FarmerCollideWithEvent : StaticContextEvent
+    {
+        public Object CollisionObject { get; }
+
+        public FarmerCollideWithEvent(ObjectAccessor collisionObject)
+        {
+            CollisionObject = new Object(StaticGameContext.WrappedGame, collisionObject);
+        }
+    }
+}
