4ed77cc18b010fec2c5c3a9556cb9be4bb878740
Cody <notdemmonic@hotmail.com>
2016-03-08 19:26:53 -0500

    added safe guards for proxy mapping

--- Storm/Storm/StardewValley/Proxy/MappedInterceptorFactory.cs
+++ Storm/Storm/StardewValley/Proxy/MappedInterceptorFactory.cs
@@ -46,6 +46,13 @@ namespace Storm.StardewValley.Proxy
                 var attr = method.GetCustomAttribute<ProxyMap>();
                 if (attr != null)
                 {
+                    var name = InjectorMetaData.NameOfMethod(accessor, injectors, attr.Name);
+                    if (name == null)
+                    {
+                        Logging.Logs("[{0}] Failed to find obfuscated name to map", GetType().Name);
+                        Logging.Logs("\t{0} {1} {2}", accessor.Name, attr.Name, method.Name);
+                        continue;
+                    }
                     callMap.Add(InjectorMetaData.NameOfMethod(accessor, injectors, attr.Name), method);
                 }
             }
