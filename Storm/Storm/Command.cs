a0ff0ee46fcce312e5de90c5a1a45b395874f91d
Zoryn Aaron <zoryn4163@gmail.com>
2016-03-09 00:05:42 -0500

    command parser class

--- Storm/Storm/Command.cs
+++ Storm/Storm/Command.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace Storm
 {
@@ -10,12 +9,8 @@ namespace Storm
 
         public static Command ParseCommand(string input)
         {
-            string[] spaces = input.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
-            if (string.Join("", spaces) == input)
-            {
-                return new Command(input);
-            }
-            return null;
+            string[] spaces = input.Split(new[] {" "}, 2, StringSplitOptions.RemoveEmptyEntries);
+            return spaces[0] == input ? new Command(input) : new Command(spaces[0].TrimStart(new[] {'.', '/'}), spaces[1].Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries));
         }
 
         public Command(string name, string[] args = null)
