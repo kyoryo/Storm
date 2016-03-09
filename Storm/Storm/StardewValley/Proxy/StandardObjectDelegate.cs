81b0ed51ece24bf39e469fb038c3fa4643ffccbd
Cody <notdemmonic@hotmail.com>
2016-03-08 19:15:26 -0500

    billboard & object delegate stuff..

---
+++ Storm/Storm/StardewValley/Proxy/StandardObjectDelegate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storm.StardewValley.Proxy
+{
+    public class StandardObjectDelegate : ObjectDelegate
+    {
+        private int parentSpriteSheetIndex;
+        private int initialStack;
+
+        public StandardObjectDelegate(int parentSpriteSheetIndex, int initialStack)
+        {
+            this.parentSpriteSheetIndex = parentSpriteSheetIndex;
+            this.initialStack = initialStack;
+        }
+
+        public override object[] GetConstructorParams()
+        {
+            return new object[] { parentSpriteSheetIndex, initialStack };
+        }
+    }
+}
