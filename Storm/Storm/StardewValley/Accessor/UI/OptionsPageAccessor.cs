b69a02661f6aab26786cdf9579ab2f693d1208fa
Cody <notdemmonic@hotmail.com>
2016-03-10 08:06:18 -0500

    options page accessor impl

---
+++ Storm/Storm/StardewValley/Accessor/UI/OptionsPageAccessor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storm.StardewValley.Accessor
+{
+    public interface OptionsPageAccessor
+    {
+        System.String _GetDescriptionText();
+        void _SetDescriptionText(System.String val);
+
+        System.String _GetHoverText();
+        void _SetHoverText(System.String val);
+
+        System.Collections.IList _GetOptionSlots();
+        void _SetOptionSlots(System.Collections.IList val);
+
+        int _GetCurrentItemIndex();
+        void _SetCurrentItemIndex(int val);
+
+        ClickableTextureComponentAccessor _GetUpArrow();
+        void _SetUpArrow(ClickableTextureComponentAccessor val);
+
+        ClickableTextureComponentAccessor _GetDownArrow();
+        void _SetDownArrow(ClickableTextureComponentAccessor val);
+
+        ClickableTextureComponentAccessor _GetScrollBar();
+        void _SetScrollBar(ClickableTextureComponentAccessor val);
+
+        bool _GetScrolling();
+        void _SetScrolling(bool val);
+
+        System.Collections.IList _GetOptions();
+        void _SetOptions(System.Collections.IList val);
+
+        Microsoft.Xna.Framework.Rectangle _GetScrollBarRunner();
+        void _SetScrollBarRunner(Microsoft.Xna.Framework.Rectangle val);
+
+        int _GetOptionsSlotHeld();
+        void _SetOptionsSlotHeld(int val);
+    }
+}
