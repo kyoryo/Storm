9e6c70feba76e6780a960684eb2b95c6c0bdf37c
Matt Stevens <matt@handsomematt.co.uk>
2016-03-14 13:19:54 +0000

    Utility methods

---
+++ Storm/Storm/StardewValley/Accessor/UtilityAccessor.cs
@@ -0,0 +1,30 @@
+﻿/*
+    Copyright 2016 Matt Stevens (Handsome Matt)
+
+    Storm is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Storm is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Storm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Storm.StardewValley.Accessor
+{
+    public interface UtilityAccessor
+    {
+        void _DrawBoldText(SpriteBatch b, string text, SpriteFont font, Vector2 position, Color color, float scale = 1f, float layerDepth = -1f, int boldnessOffset = 1);
+        void _DrawWithShadow(SpriteBatch b, Texture2D texture, Vector2 position, Rectangle sourceRect, Color color, float rotation, Vector2 origin, float scale = -1f, bool flipped = false, float layerDepth = -1f, int horizontalShadowOffset = -1, int verticalShadowOffset = -1, float shadowIntensity = 0.35f);
+        void _DrawTextWithShadow(SpriteBatch b, string text, SpriteFont font, Vector2 position, Color color, float scale = 1f, float layerDepth = -1f, int horizontalShadowOffset = -1, int verticalShadowOffset = -1, float shadowIntensity = 1f, int numShadows = 3);
+    }
+}
